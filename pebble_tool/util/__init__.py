bee4c2c38d075619a1098a85444f669ca81f7f41
Joshua Jun <lets@throw.rocks>
2026-05-22 19:41:15 +0200

    Prefer legacy ~/.pebble-sdk, fallback to XDG data dir
    Implements #49

--- pebble_tool/util/__init__.py
+++ pebble_tool/util/__init__.py
@@ -8,7 +8,13 @@ def get_persist_dir():
     if platform.system() == 'Darwin':
         dir = os.path.expanduser("~/Library/Application Support/Pebble SDK")
     else:
-        dir = os.path.expanduser("~/.pebble-sdk")
+        legacy_dir = os.path.expanduser("~/.pebble-sdk")
+        if os.path.exists(legacy_dir):
+            dir = legacy_dir
+        else:
+            # Follow the XDG Base Directory specification for new installations.
+            data_home = os.environ.get('XDG_DATA_HOME') or os.path.expanduser("~/.local/share")
+            dir = os.path.join(data_home, "pebble-sdk")
     if not os.path.exists(dir):
         os.makedirs(dir)
     return dir
